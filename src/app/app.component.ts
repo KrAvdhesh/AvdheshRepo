2ec6fc6306171c88bc4af1bbe30181aac2f5c2ed
Avdhesh Kumar Porwal <avdhesh.porwal@gmail.com>
2023-05-28 13:50:21 +0530

    trackby

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,90 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'track-by';
+  title = 'track-by performace avdhesh';
+  students:any[];
+  constructor()
+  {
+this.students=[
+  {
+StudentId:1,
+Name:'Amit',
+Gender:'Male',
+Age:27,
+Course:'MCA'
+  },
+  {
+    StudentId:2,
+    Name:'Anuj',
+    Gender:'Male',
+    Age:25,
+    Course:'BSC'
+      },
+      {
+        StudentId:3,
+        Name:'Sachin',
+        Gender:'Male',
+        Age:21,
+        Course:'BBA'
+          },
+          {
+            StudentId:4,
+            Name:'Sheetal',
+            Gender:'Female',
+            Age:22,
+            Course:'BCA'
+              }
+
+];
+
+  }
+
+  getMoreStudent():void
+  {
+    this.students=[
+      {
+    StudentId:1,
+    Name:'Amit',
+    Gender:'Male',
+    Age:27,
+    Course:'MCA'
+      },
+      {
+        StudentId:2,
+        Name:'Anuj',
+        Gender:'Male',
+        Age:25,
+        Course:'BSC'
+          },
+          {
+            StudentId:3,
+            Name:'Sachin',
+            Gender:'Male',
+            Age:21,
+            Course:'BBA'
+              },
+              {
+                StudentId:4,
+                Name:'Sheetal',
+                Gender:'Female',
+                Age:22,
+                Course:'BCA'
+                  },
+                  {
+                    StudentId:5,
+                    Name:'AArav',
+                    Gender:'male',
+                    Age:11,
+                    Course:'6th'
+                      }
+    
+    ];
+    
+
+  }
+
+  trackbystudentId(student:any):string
+  {
+    return student.StudentId;
+  }
 }
